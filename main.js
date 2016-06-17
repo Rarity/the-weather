c49a806842e5beab276ab1ff876a24ac49425b59
Rarity <xaos.92@gmail.com>
2016-06-17 11:32:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var city = "moscow";
 $(document).ready(function() {
 	$("body").css("display","none").fadeIn(500);	
 	$.getJSON("http://api.openweathermap.org/data/2.5/weather?apikey="+apiKey+"&q="+city+"&units=metric", function(json) {
-		var temp = -2;
+		var temp = json.temp;
 		var city = json.name;
 		var icon = json.weather[0].icon;
 		
